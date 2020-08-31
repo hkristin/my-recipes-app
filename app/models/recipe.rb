733f623bf3ad46bbbeb8c8dd4029ed756552ed8d
Kristin Howard <kristinhoward@Kristins-MacBook-Pro.local>
2020-08-31 12:17:20 -0400

    fixed issue where newly created recipe wouldn't save before executing custom cookbook_recipes_attributes=() setter

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,6 +1,5 @@
 class Recipe < ApplicationRecord
-  has_many :cookbook_recipes
-  # , dependent: :destroy
+  has_many :cookbook_recipes, dependent: :destroy
   has_many :cookbooks, :through => :cookbook_recipes
 
   validates :name, presence: true
@@ -9,7 +8,8 @@ class Recipe < ApplicationRecord
 
   def cookbook_recipes_attributes=(cookbook_recipe)
     if !cookbook_recipe[:prep_time].blank? || !cookbook_recipe[:cook_time].blank?
-      new_cookbook_recipe = self.cookbook_recipes.find_by(cookbook_id: cookbook_recipe["cookbook_id"], recipe_id: self.id)
+      self.save if self.id.nil?
+      new_cookbook_recipe = self.cookbook_recipes.find_or_create_by(cookbook_id: cookbook_recipe["cookbook_id"].to_i, recipe_id: self.id)
       new_cookbook_recipe.update(prep_time: cookbook_recipe[:prep_time], cook_time: cookbook_recipe[:cook_time])
     end
   end
